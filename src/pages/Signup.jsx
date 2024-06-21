7c6be300261f48ebd0bf9d832e532fe5b7d22b6e
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-06-22 00:30:49 +0530

    signup validation

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,52 @@
+import { useState } from "react";
+import signupValidator from "../validators/signupValidator";
+
+const initialFormData = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+const initialFormError = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 const Signup = () => {
+  const [formData, setFormData] = useState(initialFormData);
+  const [formError, setFormError] = useState(initialFormError);
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = signupValidator({
+      name: formData.name,
+      email: formData.email,
+      password: formData.password,
+      confirmPassword: formData.confirmPassword,
+    });
+
+    if (
+      errors.name ||
+      errors.email ||
+      errors.password ||
+      errors.confirmPassword
+    ) {
+      setFormError(errors);
+    } else {
+      setFormError(initialFormError);
+    }
+    console.log(formData);
+  };
+
   return (
     <div className="form-container">
-      <form className="inner-container">
+      <form className="inner-container" onSubmit={handleSubmit}>
         <h2 className="form-title">Signup Form</h2>
         <div className="form-group">
           <label>Name</label>
@@ -10,7 +55,10 @@ const Signup = () => {
             type="text"
             name="name"
             placeholder="Jhon Doe"
+            value={formData.name}
+            onChange={handleChange}
           />
+          {formError.name && <p className="error">{formError.name}</p>}
         </div>
 
         <div className="form-group">
@@ -20,7 +68,10 @@ const Signup = () => {
             type="text"
             name="email"
             placeholder="doe@gmail.com"
+            value={formData.email}
+            onChange={handleChange}
           />
+          {formError.email && <p className="error">{formError.email}</p>}
         </div>
 
         <div className="form-group">
@@ -30,7 +81,10 @@ const Signup = () => {
             type="password"
             name="password"
             placeholder="***********"
+            value={formData.password}
+            onChange={handleChange}
           />
+          {formError.password && <p className="error">{formError.password}</p>}
         </div>
 
         <div className="form-group">
@@ -40,7 +94,12 @@ const Signup = () => {
             type="password"
             name="confirmPassword"
             placeholder="***********"
+            value={formData.confirmPassword}
+            onChange={handleChange}
           />
+          {formError.confirmPassword && (
+            <p className="error">{formError.confirmPassword}</p>
+          )}
         </div>
 
         <div className="form-group">
