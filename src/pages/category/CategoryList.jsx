0e1fc9dbc40e9a1b38c4b955f4a89644cff407ce
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-06-24 02:16:02 +0530

    category list with pagination

--- src/pages/category/CategoryList.jsx
+++ src/pages/category/CategoryList.jsx
@@ -1,142 +1,175 @@
 import { useNavigate } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import axios from "../../utils/axiosInstance";
+import { toast } from "react-toastify";
+import moment from "moment";
 
 const CategoryList = () => {
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [totalPage, setTotalPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageCount, setPageCount] = useState([]);
+  const [searchValue,setSearchValue] = useState("")
 
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        setLoading(true);
+
+        //api request
+        const response = await axios.get(`/category?page=${currentPage}&q=${searchValue}`);
+        const data = response.data.data;
+        setCategories(data.categories);
+        setTotalPage(data.pages);
+
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        const response = error.response;
+        const data = response.data;
+
+        toast.error(data.message);
+      }
+    };
+    getCategories();
+  }, [currentPage]);
+
+  useEffect(() => {
+    if (totalPage > 1) {
+      let tempPageCount = [];
+
+      for (let i = 1; i <= totalPage; i++) {
+        tempPageCount = [...tempPageCount, i];
+      }
+
+      setPageCount(tempPageCount);
+    } else {
+      setPageCount([]);
+    }
+  }, [totalPage]);
+
+
+  const handlePrev = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
+
+  const handleNext = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
+
+  const handlePage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handleSearch = async (e) => {
+    try {
+      const input = e.target.value;
+
+      setSearchValue(input);
+
+      const response = await axios.get(
+        `/category?q=${input}&page=${currentPage}`
+      );
+      const data = response.data.data;
+
+      setCategories(data.categories);
+      setTotalPage(data.pages);
+    } catch (error) {
+      const response = error.response;
+      const data = response.data;
+      toast.error(data.message);
+    }
+  };
+
   return (
     <div>
-      <button className="button button-block" onClick={()=>navigate("new-category")}>Add New Category</button>
+      <button
+        className="button button-block"
+        onClick={() => navigate("new-category")}
+      >
+        Add New Category
+      </button>
       <h2 className="table-title">Category list</h2>
       <input
-        className="saerch-input"
+        className="search-input"
         type="text"
         name="search"
         placeholder="Search here"
+        onChange={handleSearch}
       />
 
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Created At</th>
-            <th>Updated At</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Category 1</td>
-            <td>Test Description</td>
-            <td>2023-10-01 14:43:52</td>
-            <td>2023-10-01 14:43:52</td>
-            <th>
-              <button className="button" onClick={()=>navigate("update-category")}>Update</button>
-              <button className="button">Delete</button>
-            </th>
-          </tr>
-          <tr>
-            <td>Category 2</td>
-            <td>Test Description</td>
-            <td>2023-10-01 14:43:52</td>
-            <td>2023-10-01 14:43:52</td>
-            <th>
-              <button className="button">Update</button>
-              <button className="button">Delete</button>
-            </th>
-          </tr>
-          <tr>
-            <td>Category 3</td>
-            <td>Test Description</td>
-            <td>2023-10-01 14:43:52</td>
-            <td>2023-10-01 14:43:52</td>
-            <th>
-              <button className="button">Update</button>
-              <button className="button">Delete</button>
-            </th>
-          </tr>
-          <tr>
-            <td>Category 4</td>
-            <td>Test Description</td>
-            <td>2023-10-01 14:43:52</td>
-            <td>2023-10-01 14:43:52</td>
-            <th>
-              <button className="button">Update</button>
-              <button className="button">Delete</button>
-            </th>
-          </tr>
-          <tr>
-            <td>Category 5</td>
-            <td>Test Description</td>
-            <td>2023-10-01 14:43:52</td>
-            <td>2023-10-01 14:43:52</td>
-            <th>
-              <button className="button">Update</button>
-              <button className="button">Delete</button>
-            </th>
-          </tr>
-          <tr>
-            <td>Category 6</td>
-            <td>Test Description</td>
-            <td>2023-10-01 14:43:52</td>
-            <td>2023-10-01 14:43:52</td>
-            <th>
-              <button className="button">Update</button>
-              <button className="button">Delete</button>
-            </th>
-          </tr>
-          <tr>
-            <td>Category 7</td>
-            <td>Test Description</td>
-            <td>2023-10-01 14:43:52</td>
-            <td>2023-10-01 14:43:52</td>
-            <th>
-              <button className="button">Update</button>
-              <button className="button">Delete</button>
-            </th>
-          </tr>
-          <tr>
-            <td>Category 8</td>
-            <td>Test Description</td>
-            <td>2023-10-01 14:43:52</td>
-            <td>2023-10-01 14:43:52</td>
-            <th>
-              <button className="button">Update</button>
-              <button className="button">Delete</button>
-            </th>
-          </tr>
-          <tr>
-            <td>Category 9</td>
-            <td>Test Description</td>
-            <td>2023-10-01 14:43:52</td>
-            <td>2023-10-01 14:43:52</td>
-            <th>
-              <button className="button">Update</button>
-              <button className="button">Delete</button>
-            </th>
-          </tr>
-          <tr>
-            <td>Category 10</td>
-            <td>Test Description</td>
-            <td>2023-10-01 14:43:52</td>
-            <td>2023-10-01 14:43:52</td>
-            <th>
-              <button className="button">Update</button>
-              <button className="button">Delete</button>
-            </th>
-          </tr>
-        </tbody>
-      </table>
+      {loading ? (
+        "loading...."
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Created At</th>
+              <th>Updated At</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categories.map((category) => (
+              <tr key={category._id}>
+                <td>{category.title}</td>
+                <td>{category.desc}</td>
+                <td>
+                  {moment(category.createdAt).format("YYYY-MM-DD HH:mm:ss")}
+                </td>
+                <td>
+                  {moment(category.updatedAt).format("YYYY-MM-DD HH:mm:ss")}
+                </td>
+                <th>
+                  <button
+                    className="button"
+                    onClick={() => navigate("update-category")}
+                  >
+                    Update
+                  </button>
+                  <button className="button">Delete</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
 
-      <div className="pag-container">
-        <button className="pag-button">prev</button>
-        <button className="pag-button">1</button>
-        <button className="pag-button">2</button>
-        <button className="pag-button">3</button>
-        <button className="pag-button">next</button>
-      </div>
+      {pageCount.length > 0 && (
+        <div className="pag-container">
+          <button
+            className="pag-button"
+            onClick={handlePrev}
+            disabled={currentPage === 1}
+          >
+            prev
+          </button>
+          {pageCount.map((pageNumber, index) => (
+            <button
+              className="pag-button"
+              key={index}
+              onClick={() => handlePage(pageNumber)}
+              style={{
+                backgroundColor: currentPage === pageNumber ? "#ccc" : "",
+              }}
+            >
+              {pageNumber}
+            </button>
+          ))}
+          <button
+            className="pag-button"
+            onClick={handleNext}
+            disabled={currentPage === totalPage}
+          >
+            next
+          </button>
+        </div>
+      )}
     </div>
   );
 };
