90d2f085dbb3cdae4e01392040895f74d4d23495
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-06-23 23:56:37 +0530

    add token to headers

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,4 +1,18 @@
 import axios from "axios";
-const axiosInstance = axios.create({baseURL:"http://127.0.0.1:8000/api/v1"})
+
+const axiosInstance = axios.create({ baseURL: "http://localhost:8000/api/v1" });
+
+axiosInstance.interceptors.request.use((req) => {
+  const stringifyBlogData = window.localStorage.getItem("blogData");
+
+  if (stringifyBlogData) {
+    const blogData = JSON.parse(stringifyBlogData);
+    const token = blogData.token;
+
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
+});
 
 export default axiosInstance;
