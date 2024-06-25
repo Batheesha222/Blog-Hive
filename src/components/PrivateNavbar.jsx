f47a5e45565aca08e596eff8abe5b004866fc5e6
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-06-25 19:53:58 +0530

    multi roles

--- src/components/PrivateNavbar.jsx
+++ src/components/PrivateNavbar.jsx
@@ -1,9 +1,11 @@
 import { NavLink } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
 
 const PrivateNavbar = () => {
   const navigate = useNavigate();
+  const auth = useAuth();
 
   const handleLogout = () => {
     window.localStorage.removeItem("blogData");
@@ -12,14 +14,18 @@ const PrivateNavbar = () => {
   };
   return (
     <nav className="primary-link">
-        <NavLink to="/">Home</NavLink>
+      <NavLink to="/">Home</NavLink>
+      {(auth.role === 1 || auth.role === 2) && (
         <NavLink to="/categories">categories</NavLink>
-        <NavLink to="/posts">posts</NavLink>
-        <NavLink to="/profile">profile</NavLink>
-        <NavLink to="/setting">setting</NavLink>
-        <NavLink to="/login" onClick={handleLogout}>Logout</NavLink>
+      )}
+      <NavLink to="/posts">posts</NavLink>
+      <NavLink to="/profile">profile</NavLink>
+      <NavLink to="/setting">setting</NavLink>
+      <NavLink to="/login" onClick={handleLogout}>
+        Logout
+      </NavLink>
     </nav>
-  )
-}
+  );
+};
 
-export default PrivateNavbar
+export default PrivateNavbar;
